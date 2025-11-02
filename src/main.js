dfa85de4f543f98c154625efad505822395f1edd
Juan Alberto Cuevas Juarez <136052531+juan-albertoC@users.noreply.github.com>
2025-11-02 16:21:10 -0600

    crear carpeta assets en src/assets  modificar archivo src/main.js para importar los estilos ya que se han copiado todos los estilos a la carpeta assets

--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+// Importar los estilos globales
+import './assets/reset.css'
+import './assets/style.css'
+import './assets/productos.css'
+
 import router from './router'
 
 const app = createApp(App)
