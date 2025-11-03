2aa6aa2ac8a402d704578e332b20e5bbaf62e416
Juan Alberto Cuevas Juarez <136052531+juan-albertoC@users.noreply.github.com>
2025-11-03 01:06:57 -0600

    Hacer Home.vue y Productos.vue dinamicos e interactivos

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,20 +1,47 @@
 
 
 <template>
-  <HeaderBar />
-  <BannerImage />
-  <PrincipalSection />
-  <DiferencialesSection />
+  <div>
+    <HeaderBar />
+
+    <section class="principal">
+      <h2>Sobre la Barbería Alura</h2>
+      <p>{{ descripcion }}</p>
+
+      <h3>Diferenciales</h3>
+      <ul>
+        <li v-for="(item, index) in diferenciales" :key="index">{{ item }}</li>
+      </ul>
+
+      <button @click="agregarDiferencial">Agregar diferencial</button>
+    </section>
+
+    <FooterBar />
+  </div>
 </template>
 
 
 <script setup>
 
-   import HeaderBar from '../components/HeaderBar.vue'
-   import BannerImage from '../components/BannerImage.vue'
-   import PrincipalSection from '../components/PrincipalSection.vue'
-   import DiferencialesSection from '../components/DiferencialesSection.vue'
+import { ref } from 'vue'
+import HeaderBar from '../components/HeaderBar.vue'
+import FooterBar from '../components/FooterBar.vue'
 
+//  Variables reactivas
+const descripcion = ref(
+  'Ubicada en el corazón de la ciudad, la Barbería Alura ofrece lo mejor para tu cabello y barba.'
+)
+
+const diferenciales = ref([
+  'Atención personalizada',
+  'Espacio diferenciado',
+  'Localización',
+  'Profesionales calificados'
+])
+
+function agregarDiferencial() {
+  diferenciales.value.push('Nuevo servicio agregado')
+}
 </script>
 
 <style scoped></style>
--- src/views/Productos.vue
+++ src/views/Productos.vue
@@ -1,28 +1,22 @@
 <template>
   <div>
-    <HeaderNav />
+    <HeaderBar />
 
     <main>
+      <h2>Productos disponibles</h2>
+
       <ul class="productos">
         <ProductItem
-          title="Cabello"
-          image="/imagenes/cabello.jpg"
-          description="Con tijera o máquina, a gusto del cliente"
-          price="$10.00"
-        />
-        <ProductItem
-          title="Barba"
-          image="/imagenes/barba.jpg"
-          description="Corte y diseño profesional de barba"
-          price="$8.00"
-        />
-        <ProductItem
-          title="Cabello + Barba"
-          image="/imagenes/cabello+barba.jpg"
-          description="Paquete completo de cabello y barba"
-          price="$15.00"
+          v-for="(producto, index) in productos"
+          :key="index"
+          :title="producto.title"
+          :image="producto.image"
+          :description="producto.description"
+          :price="producto.price"
         />
       </ul>
+
+      <button @click="ordenarPorPrecio">Ordenar por precio</button>
     </main>
 
     <FooterBar />
@@ -30,9 +24,37 @@
 </template>
 
 <script setup>
-   import HeaderNav from '../components/HeaderNav.vue'
-   import ProductItem from '../components/ProductItem.vue'
-   import FooterBar from '../components/FooterBar.vue'
+  import { ref } from 'vue'
+import HeaderBar from '../components/HeaderBar.vue'
+import ProductItem from '../components/ProductItem.vue'
+import FooterBar from '../components/FooterBar.vue'
+
+//  Estado reactivo
+const productos = ref([
+  {
+    title: 'Cabello',
+    image: '/imagenes/cabello.jpg',
+    description: 'Con tijera o máquina, a gusto del cliente',
+    price: 10.0
+  },
+  {
+    title: 'Barba',
+    image: '/imagenes/barba.jpg',
+    description: 'Corte y diseño profesional de barba',
+    price: 8.0
+  },
+  {
+    title: 'Cabello + Barba',
+    image: '/imagenes/cabello+barba.jpg',
+    description: 'Paquete completo de cabello y barba',
+    price: 15.0
+  }
+])
+
+//  Interactividad
+function ordenarPorPrecio() {
+  productos.value.sort((a, b) => a.price - b.price)
+}
 </script>
 
 <style scoped>
